df099b64c05ec2a78ec7fddd424d4225589d6334
luis-trevisan <lpst@cin.ufpe.br>
2022-03-19 20:05:53 -0300

    working again

--- kernel.asm
+++ kernel.asm
@@ -2,13 +2,13 @@ org 0x7e00
 jmp 0x0000:start
 
 data:
-    mensagem db 'Digite uma palavra de 6 letras: ', 0
-    palavra db 'batata', 0
-    sucesso db 'Parabens!! Voce conseguiu', 0
-    falha db 'Ops, nao foi dessa vez :/', 0
-    X times 10 db 0
+  mensagem db 'Digite uma palavra de 6 letras: ', 0
+  palavra db 'batata', 0
+  sucesso db 'Parabens!! Voce conseguiu', 0
+  falha db 'Ops, nao foi dessa vez :/', 0
+  X times 10 db 0
 
-    putchar:
+putchar:
   mov ah, 0x0e
   int 10h
   ret
@@ -44,48 +44,6 @@ prints:             ; mov si, string
   .endloop:
   ret
   
-reverse:              ; mov si, string
-  mov di, si
-  xor cx, cx          ; zerar contador
-  .loop1:             ; botar string na stack
-    lodsb
-    cmp al, 0
-    je .endloop1
-    inc cl
-    push ax
-    jmp .loop1
-  .endloop1:
-  .loop2:             ; remover string da stack        
-    pop ax
-    stosb
-    loop .loop2
-  ret
-  
-tostring:              ; mov ax, int / mov di, string
-  push di
-  .loop1:
-    cmp ax, 0
-    je .endloop1
-    xor dx, dx
-    mov bx, 10
-    div bx            ; ax = 9999 -> ax = 999, dx = 9
-    xchg ax, dx       ; swap ax, dx
-    add ax, 48        ; 9 + '0' = '9'
-    stosb
-    xchg ax, dx
-    jmp .loop1
-  .endloop1:
-  pop si
-  cmp si, di
-  jne .done
-  mov al, 48
-  stosb
-  .done:
-  mov al, 0
-  stosb
-  call reverse
-  ret
-  
 gets:                 ; mov di, string
   xor cx, cx          ; zerar contador
   .loop1:
@@ -116,24 +74,6 @@ gets:                 ; mov di, string
   call endl
   ret
   
-stoi:                ; mov si, string
-  xor cx, cx
-  xor ax, ax
-  .loop1:
-    push ax
-    lodsb
-    mov cl, al
-    pop ax
-    cmp cl, 0        ; check EOF(NULL)
-    je .endloop1
-    sub cl, 48       ; '9'-'0' = 9
-    mov bx, 10
-    mul bx           ; 999*10 = 9990
-    add ax, cx       ; 9990+9 = 9999
-    jmp .loop1
-  .endloop1:
-  ret
-  
 strcmp:              ; mov si, string1, mov di, string2
   .loop1:
     lodsb
@@ -149,62 +89,6 @@ strcmp:              ; mov si, string1, mov di, string2
   .equal:
     stc
     ret
-
-strcpy:
-  .loop1:
-    lodsb
-    stosb
-    cmp al, 0
-    je .endloop1
-    jmp .loop1
-  .endloop1:
-  ret
-
-print_dots:              ; mov cx, number of dots
-  for:
-    mov al, '.'
-    call putchar
-    push cx
-    call delay100ms
-    call delay100ms
-    pop cx
-    loop for
-  ret
-  
-delay1s:                 ; 1 SEC DELAY
-  mov cx, 0fh
-  mov dx, 4240h
-  mov ah, 86h
-  int 15h
-  ret
-
-delay100ms:              ; 0.1 SEC DELAY
-  mov cx, 01h
-  mov dx, 86a0h
-  mov ah, 86h
-  int 15h
-  ret
-
-clear:                   ; mov bl, color
-  ; set the cursor to top left-most corner of screen
-  mov dx, 0 
-  mov bh, 0      
-  mov ah, 0x2
-  int 0x10
-
-  ; print 2000 blank chars to clean  
-  mov cx, 2000 
-  mov bh, 0
-  mov al, 0x20 ; blank char
-  mov ah, 0x9
-  int 0x10
-  
-  ; reset cursor to top left-most corner of screen
-  mov dx, 0 
-  mov bh, 0      
-  mov ah, 0x2
-  int 0x10
-  ret
 
 start:
     xor ax, ax    ;limpando ax
