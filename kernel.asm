0a0175d518a191d073340532137b20e32a060380
luis-trevisan <lpst@cin.ufpe.br>
2022-03-19 18:47:24 -0300

    add loop

--- kernel.asm
+++ kernel.asm
@@ -2,169 +2,232 @@ org 0x7e00
 jmp 0x0000:start
 
 data:
-    mensagem db 'Digite uma palavra', 0
+    mensagem db 'Digite uma palavra de 6 letras: ', 0
     palavra db 'batata', 0
+    sucesso db 'Parabens!! Voce conseguiu', 0
+    falha db 'Ops, nao foi dessa vez :/', 0
     X times 10 db 0
 
-    putchar:    ;Printa um caractere na tela, pega o valor salvo em al
-        mov ah, 0x0e
-        int 10h
-    ret
-    
-    getchar:    ;Pega o caractere lido no teclado e salva em al
-        mov ah, 0x00
-        int 16h
-    ret
-
-    delchar:    ;Deleta um caractere lido no teclado
-        mov al, 0x08          ; backspace
-        call putchar
-        mov al, ' '
-        call putchar
-        mov al, 0x08          ; backspace
-        call putchar
-    ret
+    putchar:
+  mov ah, 0x0e
+  int 10h
+  ret
   
-    endl:       ;Pula uma linha, printando na tela o caractere que representa o /n
-        mov al, 0x0a          ; line feed
-        call putchar
-        mov al, 0x0d          ; carriage return
-        call putchar
-    ret
+getchar:
+  mov ah, 0x00
+  int 16h
+  ret
+  
+delchar:
+  mov al, 0x08          ; backspace
+  call putchar
+  mov al, ' '
+  call putchar
+  mov al, 0x08          ; backspace
+  call putchar
+  ret
+  
+endl:
+  mov al, 0x0a          ; line feed
+  call putchar
+  mov al, 0x0d          ; carriage return
+  call putchar
+  ret
 
-    gets:                 ; mov di, string, salva na string apontada por di, cada caractere lido na linha   
-        xor cx, cx          ; zerar contador
-        .loop1:
-            call getchar
-            cmp al, 0x08      ; backspace
-            je .backspace
-                cmp al, 0x0d      ; carriage return
-            je .done
-                cmp cl, 10        ; string limit checker
-            je .loop1
+;  var al, *si;
+;  lodsb(){
+;    al = *si;
+;    si++;
+;  }
+
+;    var al, *di;
+;    stosb(){
+;        *di = al;
+;        di++;
+;    }
+
+prints:             ; mov si, string
+  .loop:
+    lodsb           ; bota character em al 
+    cmp al, 0
+    je .endloop
+    call putchar
+    jmp .loop
+  .endloop:
+  ret
+  
+reverse:              ; mov si, string
+  mov di, si
+  xor cx, cx          ; zerar contador
+  .loop1:             ; botar string na stack
+    lodsb
+    cmp al, 0
+    je .endloop1
+    inc cl
+    push ax
+    jmp .loop1
+  .endloop1:
+  .loop2:             ; remover string da stack        
+    pop ax
+    stosb
+    loop .loop2
+  ret
+  
+tostring:              ; mov ax, int / mov di, string
+  push di
+  .loop1:
+    cmp ax, 0
+    je .endloop1
+    xor dx, dx
+    mov bx, 10
+    div bx            ; ax = 9999 -> ax = 999, dx = 9
+    xchg ax, dx       ; swap ax, dx
+    add ax, 48        ; 9 + '0' = '9'
+    stosb
+    xchg ax, dx
+    jmp .loop1
+  .endloop1:
+  pop si
+  cmp si, di
+  jne .done
+  mov al, 48
+  stosb
+  .done:
+  mov al, 0
+  stosb
+  call reverse
+  ret
+  
+gets:                 ; mov di, string
+  xor cx, cx          ; zerar contador
+  .loop1:
+    call getchar
+    cmp al, 0x08      ; backspace
+    je .backspace
+    cmp al, 0x0d      ; carriage return
+    je .done
+    cmp cl, 10        ; string limit checker
+    je .loop1
     
-        stosb
-        inc cl
-        call putchar
+    stosb
+    inc cl
+    call putchar
     
-        jmp .loop1
-        .backspace:
-        cmp cl, 0       ; is empty?
-        je .loop1
-            dec di
-            dec cl
-            mov byte[di], 0
-            call delchar
-        jmp .loop1
-        .done:
-            mov al, 0
-            stosb
-            call endl
+    jmp .loop1
+    .backspace:
+      cmp cl, 0       ; is empty?
+      je .loop1
+      dec di
+      dec cl
+      mov byte[di], 0
+      call delchar
+    jmp .loop1
+  .done:
+  mov al, 0
+  stosb
+  call endl
+  ret
+  
+stoi:                ; mov si, string
+  xor cx, cx
+  xor ax, ax
+  .loop1:
+    push ax
+    lodsb
+    mov cl, al
+    pop ax
+    cmp cl, 0        ; check EOF(NULL)
+    je .endloop1
+    sub cl, 48       ; '9'-'0' = 9
+    mov bx, 10
+    mul bx           ; 999*10 = 9990
+    add ax, cx       ; 9990+9 = 9999
+    jmp .loop1
+  .endloop1:
+  ret
+  
+strcmp:              ; mov si, string1, mov di, string2
+  .loop1:
+    lodsb
+    cmp al, byte[di]
+    jne .notequal
+    cmp al, 0
+    je .equal
+    inc di
+    jmp .loop1
+  .notequal:
+    clc
+    ret
+  .equal:
+    stc
     ret
 
-    cmpzinho:
-        lodsb
-        cmp al, byte[di]
-        jne .notequal
-        cmp al, byte[di]
-        je .equal
-        inc di
-        .notequal:
-            clc
-            ret
-        .equal:
-            stc
-    ret
+strcpy:
+  .loop1:
+    lodsb
+    stosb
+    cmp al, 0
+    je .endloop1
+    jmp .loop1
+  .endloop1:
+  ret
 
+print_dots:              ; mov cx, number of dots
+  for:
+    mov al, '.'
+    call putchar
+    push cx
+    call delay100ms
+    call delay100ms
+    pop cx
+    loop for
+  ret
+  
+delay1s:                 ; 1 SEC DELAY
+  mov cx, 0fh
+  mov dx, 4240h
+  mov ah, 86h
+  int 15h
+  ret
 
+delay100ms:              ; 0.1 SEC DELAY
+  mov cx, 01h
+  mov dx, 86a0h
+  mov ah, 86h
+  int 15h
+  ret
 
-    print_string_color:
-        .loop:
-            lodsb           ; bota character apontado por si em al 
-            cmp al, 0       ; 0 é o valor atribuido ao final de uma string
-            je .endloop     ; Se for o final da string, acaba o loop
-            call cmpzinho   ; compara caractere
-            je .certo
-            mov ah, 09h
-            mov bl, 4
-            int 10h
-            jmp .loop
-            .certo:
-                mov ah, 09h
-                mov bl, 2
-                int 10h
-            jmp .loop       ; volta para o inicio do loop
-        .endloop:
-    ret
+clear:                   ; mov bl, color
+  ; set the cursor to top left-most corner of screen
+  mov dx, 0 
+  mov bh, 0      
+  mov ah, 0x2
+  int 0x10
 
-    strcmp:
-        .loop1:
-            lodsb
-            cmp al, byte[di]
-            je .equal
-            jne .notequal
-            cmp al, 0   ; 0 é o valor atribuido ao final de uma string
-            je .finish
-            inc di
-            jmp .loop1
-        .notequal:
-            mov ah, 09h
-            mov bl, 4
-            int 10h
-            clc
-        ret
-        .equal:
-            mov ah, 09h
-            mov bl, 2
-            int 10h
-        ret
-        .finish:
-            call endl
-            stc
-    ret
-
-    prints:
-        .loop:
-            lodsb           ; bota character apontado por si em al 
-            cmp al, 0       ; 0 é o valor atribuido ao final de uma string
-            je .endloop     ; Se for o final da string, acaba o loop
-            call putchar    ; printa o caractere
-            jmp .loop       ; volta para o inicio do loop
-        .endloop:
-    ret
-
-    clear:
-        ; set the cursor to top left-most corner of screen
-        mov dx, 0 
-        mov bh, 0      
-        mov ah, 0x2
-        int 0x10
-
-        ; print 2000 blank chars to clean  
-        mov cx, 2000 
-        mov bh, 0
-        mov al, 0x20 ; blank char
-        mov ah, 0x9
-        int 0x10
-        
-        ; reset cursor to top left-most corner of screen
-        mov dx, 0 
-        mov bh, 0      
-        mov ah, 0x2
-        int 0x10
-    ret
+  ; print 2000 blank chars to clean  
+  mov cx, 2000 
+  mov bh, 0
+  mov al, 0x20 ; blank char
+  mov ah, 0x9
+  int 0x10
+  
+  ; reset cursor to top left-most corner of screen
+  mov dx, 0 
+  mov bh, 0      
+  mov ah, 0x2
+  int 0x10
+  ret
 
 start:
     xor ax, ax    ;limpando ax
     mov ds, ax    ;limpando ds
     mov es, ax    ;limpando es
 
+.leitura:
     ;Imprimindo na tela a mensagem declarada em data
     mov si, mensagem    ;si aponta para o começo do endereço onde está mensagem
     call prints         ;Como só é impresso um caractere por vez, pegamos uma string com N caracteres e printamos um por um em ordem até chegar ao caractere de valor 0 que é o fim da string, assim prints pega a string para qual o ponteiro si aponta e a imprime na tela até o seu final
-    call endl           ;Pula uma linha, assim o próximo caractere imprimido estará na linha de baixo
 
-.leitura:
     ;Lendo o valor de X
     mov di, X           ;di aponta para o começo do endereço onde está X
     call gets           ;gets salva no endereço apontado por di cada caractere lido do teclado até o enter
@@ -172,11 +235,18 @@ start:
 
     mov si, X
     mov di, palavra
-    call print_string_color
-    je done             ;se for igual, ganhou (não entrou no notequal)
-
+    call strcmp
+    je .sucesso
+    mov si, falha
+    call prints
     call endl
-    jmp .leitura        ;se não for igual, tenta dnv
+    call endl
+    jmp .leitura
+
+    .sucesso:
+        mov si, sucesso
+        call prints
+        call endl
 
 done:
     jmp $
